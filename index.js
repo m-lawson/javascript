1b2f263cbe024fefcb57716a7c45d6a67fd15789
m-lawson <michellejeanlawson@gmail.com>
2014-12-12 12:02:29 -0500

    fixing commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var fileName="hellofile.txt";
+var textToWrite= "Hello World";
+
+fs.appendFile(fileName, textToWrite + '\n', function () {
+  console.log("File " + fileName + " had text' " + textToWrite + "'appended");
+});
