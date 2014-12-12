320e19aede5061b42bf3ca44ab26424d14711358
m-lawson <michellejeanlawson@gmail.com>
2014-12-12 14:11:09 -0500

    Assignment 2 revisited

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var fs= require('fs');
+var fs=require('fs');
 var randomWords = require('random-words');
 
 fs.appendFile('random.txt', randomWords({ min: 3, max: 10 }) + "\n", function (err){
-console.log("Random text added.")});
+    console.log("Random text added.");
+    ReadNumLinesInFile();
+});
 
+ReadNumLinesInFile();
+    
+function ReadNumLinesInFile(){
+    fs.readFile('random.txt', 'utf8', function(err,data) {
+        console.log("Contents of my file: " + data);
+
+    });
+}
